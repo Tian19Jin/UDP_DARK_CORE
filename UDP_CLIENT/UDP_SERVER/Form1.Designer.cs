350ced8fbd0dfe73bb55d592ccea6e4716dacce6
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2026-01-22 20:55:07 +0100

    UDP_Server

---
+++ UDP_CLIENT/UDP_SERVER/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace UDP_SERVER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblLocalPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.ChCoordenades = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.ChCoordenades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLocalPort
+            // 
+            this.lblLocalPort.AutoSize = true;
+            this.lblLocalPort.Location = new System.Drawing.Point(13, 13);
+            this.lblLocalPort.Name = "lblLocalPort";
+            this.lblLocalPort.Size = new System.Drawing.Size(72, 17);
+            this.lblLocalPort.TabIndex = 0;
+            this.lblLocalPort.Text = "Local Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(16, 47);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(196, 22);
+            this.txtPort.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(16, 131);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(88, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(16, 193);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(88, 23);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // ChCoordenades
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.ChCoordenades.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.ChCoordenades.Legends.Add(legend4);
+            this.ChCoordenades.Location = new System.Drawing.Point(159, 108);
+            this.ChCoordenades.Name = "ChCoordenades";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.ChCoordenades.Series.Add(series4);
+            this.ChCoordenades.Size = new System.Drawing.Size(559, 300);
+            this.ChCoordenades.TabIndex = 4;
+            this.ChCoordenades.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChCoordenades);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblLocalPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChCoordenades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLocalPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChCoordenades;
+    }
+}
+
---
+++ UDP_CLIENT/UDP_SERVER/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace UDP_SERVER
+{
+    public partial class Form1 : Form
+    {
+
+        private UdpClient udpServer;
+        private Thread listenThread;
+        private bool listening = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+
+            ChCoordenades.Series.Clear();
+            var series = new Series("Coordinates");
+            series.ChartType = SeriesChartType.Line;
+            ChCoordenades.Series.Add(series);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int portLocal = int.Parse(txtPort.Text);
+                udpServer = new UdpClient(portLocal);
+                listening = true;
+
+                listenThread = new Thread(ListenForMessages);
+                listenThread.IsBackground = true;
+                listenThread.Start();
+
+                MessageBox.Show($"Servidor escoltant al port {portLocal}...");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            listening = false;
+
+            if (udpServer != null)
+            {
+                udpServer.Close();
+                udpServer = null;
+            }
+
+            if (listenThread != null)
+            {
+                listenThread.Join();
+                listenThread = null;
+            }
+
+            MessageBox.Show("Servidor desconnectat.");
+        }
+
+        private void ListenForMessages()
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            while (listening)
+            {
+                try
+                {
+                    byte[] data = udpServer.Receive(ref remoteEP);
+                    string message = Encoding.UTF8.GetString(data);
+
+                    string[] parts = message.Split(',');
+                    if (parts.Length == 2 &&
+                        int.TryParse(parts[0], out int x) &&
+                        int.TryParse(parts[1], out int y))
+                    {
+                        ChCoordenades.Series["Coordinates"].Points.AddXY(y, x);
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
