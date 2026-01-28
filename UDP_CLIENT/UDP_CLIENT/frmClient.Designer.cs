13f3470bf91828c8a4c3890b99b05380fdbfd103
Tian19Jin <tian.jin@sarria.salesians.cat>
2026-01-28 19:56:33 +0100

    hacer udp y cambiar el nombre del frm

--- UDP_CLIENT/UDP_CLIENT/frmClient.Designer.cs
+++ UDP_CLIENT/UDP_CLIENT/frmClient.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace UDP_CLIENT
 {
-    partial class Form1
+    partial class frmClient
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,20 +29,137 @@ namespace UDP_CLIENT
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dtg = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtPPort);
+            this.panel1.Controls.Add(this.txtIP);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(168, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(817, 254);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(361, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PORT";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.Location = new System.Drawing.Point(119, 73);
+            this.txtIP.Multiline = true;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(219, 37);
+            this.txtIP.TabIndex = 4;
+            // 
+            // txtPPort
+            // 
+            this.txtPPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPPort.Location = new System.Drawing.Point(484, 73);
+            this.txtPPort.Multiline = true;
+            this.txtPPort.Name = "txtPPort";
+            this.txtPPort.Size = new System.Drawing.Size(219, 37);
+            this.txtPPort.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(180, 375);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(213, 95);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dtg
+            // 
+            this.dtg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg.Location = new System.Drawing.Point(513, 375);
+            this.dtg.Name = "dtg";
+            this.dtg.RowHeadersWidth = 62;
+            this.dtg.RowTemplate.Height = 28;
+            this.dtg.Size = new System.Drawing.Size(520, 266);
+            this.dtg.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 517);
+            this.ClientSize = new System.Drawing.Size(1206, 693);
+            this.Controls.Add(this.dtg);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FrmClient";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dtg;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- UDP_CLIENT/UDP_CLIENT/frmClient.cs
+++ UDP_CLIENT/UDP_CLIENT/frmClient.cs
@@ -4,18 +4,88 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UDP_CLIENT
 {
-    public partial class Form1 : Form
+    public partial class frmClient : Form
     {
-        public Form1()
+        public frmClient()
         {
             InitializeComponent();
+            dtg.ColumnCount = 2;
+            dtg.Columns[0].Name = "Valor Y";
+            dtg.Columns[1].Name = "Valor X";
+        }
+        private UdpClient udpClient;
+        private IPEndPoint remoteEndPoint;
+
+        int y = 1;
+        int max = 20;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (y > max)
+            {
+                timer1.Stop();
+                udpClient.Close();
+                btnStart.Enabled = true;
+                MessageBox.Show("Se han enviado los 20 datos.");
+            }
+
+            int x = (2 * y) + 1;
+
+            string mensaje = $"{y},{x}";
+            byte[] data = Encoding.UTF8.GetBytes(mensaje);
+
+            try
+            {
+                // 3. Enviar datos
+                udpClient.Send(data, data.Length, remoteEndPoint);
+
+
+                dtg.Rows.Add(y, x);
+
+            }
+            catch (Exception ex)
+            {
+                timer1.Stop();
+                MessageBox.Show("Error al enviar: " + ex.Message);
+            }
+
+            y++;
+        }
+    
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text;
+            int port = int.Parse(txtPPort.Text);
+
+            try
+            {
+                udpClient = new UdpClient();
+                remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                y = 1;
+                dtg.Rows.Clear(); 
+
+                btnStart.Enabled = false;
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
